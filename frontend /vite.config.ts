e3fc7b2f8244e17e7187d5115c77b57716bac59b
Sourav <souravbhardwaj2005@gmail.com>
2026-03-22 19:10:31 +0530

    fixing vercel

--- frontend /vite.config.ts
+++ frontend /vite.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 3000,
-    hmr: {
-      overlay: false,
-    },
-  },
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+});
